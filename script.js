4d57a3c5c7bf1cbac1ae04270961e4ec0a905574
LGZINDOGRAU <luis.kroessin@escola.pr.gov.br>
2026-02-08 11:55:33 -0300

    Add hover sound effect on mouse enter

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const som = document.getElementById("hoverSound");
+
+document.querySelectorAll("[data-sound]").forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        som.currentTime = 0;
+        som.play();
+    });
+});
 
